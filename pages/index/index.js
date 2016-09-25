7281b88bfb2f9e72905f8fa52593930ede2a0ff6
liuyy <953788477@qq.com>
2016-09-25 11:56:15 +0800

    首页增加加载更多的功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,19 +1,45 @@
 //index.js
 //获取应用实例
 var app = getApp()
+var utils = require('../../utils/util.js')
 Page({
   data: {
     list: [],
     duration: 2000,
     indicatorDots: true,
     autoplay: true,
-    interval: 3000
+    interval: 3000,
+    loading: false,
+    plain: false
   },
   //事件处理函数
   bindViewTap: function(e) {
     wx.navigateTo({
       url: '../detail/detail?id=' + e.target.dataset.id
     })
+  },
+  loadMore: function (e) {
+    if (this.data.list.length === 0) return
+    var date = this.getNextDate()
+    var that = this
+    that.setData({ loading: true })
+    wx.request({
+      url: 'http://news.at.zhihu.com/api/4/news/before/' + (Number(utils.formatDate(date)) + 1),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      success: function (res) {
+         that.setData({
+           loading: false,
+           list: that.data.list.concat([{ header: utils.formatDate(date, '-') }]).concat(res.data.stories)
+         })
+      }
+    })
+  },
+  getNextDate: function (){
+    var now = new Date()
+    now.setDate(now.getDate() - this.index++)
+    return now
   },
   onLoad: function () {
     var that = this
@@ -25,10 +51,11 @@ Page({
       success: function (res) {
          that.setData({
            banner: res.data.top_stories,
-           list: res.data.stories
+           list: [{ header: '今日热闻' }].concat(res.data.stories)
          })
       }
     })
+    this.index = 1
     //调用应用实例的方法获取全局数据
     // app.getUserInfo(function(userInfo){
     //   //更新数据
@@ -36,5 +63,6 @@ Page({
     //     userInfo:userInfo
     //   })
     // })
+    
   }
 })
--- utils/util.js
+++ utils/util.js
@@ -11,11 +11,20 @@ function formatTime(date) {
   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
+function formatDate(date, split) {
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
+  return [year, month, day].map(formatNumber).join(split || '')
+}
+
+
 function formatNumber(n) {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
 
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  formatDate: formatDate
 }
