5a4798ada7633ee8edd92ff35a895cb8d4ea5d9e
liuyy <953788477@qq.com>
2016-10-14 12:14:41 +0800

    add detail compiler

--- pages/index/index.js
+++ pages/index/index.js
@@ -13,12 +13,12 @@ Page({
     plain: false
   },
   //事件处理函数
-  bindViewTap: function(e) {
+  bindViewTap(e) {
     wx.navigateTo({
       url: '../detail/detail?id=' + e.target.dataset.id
     })
   },
-  loadMore: function (e) {
+  loadMore (e) {
     if (this.data.list.length === 0) return
     var date = this.getNextDate()
     var that = this
@@ -28,7 +28,7 @@ Page({
       headers: {
         'Content-Type': 'application/json'
       },
-      success: function (res) {
+      success (res) {
          that.setData({
            loading: false,
            list: that.data.list.concat([{ header: utils.formatDate(date, '-') }]).concat(res.data.stories)
@@ -36,19 +36,19 @@ Page({
       }
     })
   },
-  getNextDate: function (){
-    var now = new Date()
+  getNextDate (){
+    const now = new Date()
     now.setDate(now.getDate() - this.index++)
     return now
   },
-  onLoad: function () {
-    var that = this
+  onLoad () {
+    let that = this
     wx.request({
       url: 'http://news-at.zhihu.com/api/4/news/latest',
       headers: {
         'Content-Type': 'application/json'
       },
-      success: function (res) {
+      success (res) {
          that.setData({
            banner: res.data.top_stories,
            list: [{ header: '今日热闻' }].concat(res.data.stories)
