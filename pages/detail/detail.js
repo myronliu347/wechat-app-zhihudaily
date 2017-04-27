f5e26c91178bb302b71cd457b3065e0e33139543
LAPA\A4YL9ZZ <pxu3@mmm.com>
2017-04-27 18:30:05 +0800

    增加HTML A标签的事件处理。

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -25,5 +25,22 @@ Page({
          })
       }
     })
+  },
+  wxParseTagATap: function(e){
+    //页面URL点击处理
+    var href = e.currentTarget.dataset.src;
+    console.log(href);
+    //var ids = href.split("/");
+    //我们可以在这里进行一些路由处理
+    //wx.navigateTo({
+    //  url: '../detail/detail?id=' + ids[ids.length-1]
+    //})
+    //if(href.indexOf(index) > 0){
+      // wx.redirectTo({
+      //   url: '../index/index'
+      // })
+
+    //}
+
   }
 })
